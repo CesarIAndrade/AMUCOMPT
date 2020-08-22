9c938e86adeb7bdeb2cf6feef28d61033a2cdf0d
cesar.andrade1 <cesar.andrade@nodel.co>
2020-08-22 10:15:23 -0500

    app builded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true
 };
 
-// export const apiUrl = "http://192.168.0.10/api/";
+export const apiUrl = "http://192.168.0.10/api/";
+export const reportsUrl = "http://192.168.0.10/";
+
 // export const apiUrl = "http://localhost/api/";
-export const apiUrl = "http://25.39.0.74:90/api/";
+// export const apiUrl = "http://25.39.0.74:90/api/";
