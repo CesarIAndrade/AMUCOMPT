6202c3d1ff18f0b7b000b2312a82b2f8f3b07a78
cesar.andrade1 <cesar.andrade@nodel.co>
2021-02-22 21:11:59 -0500

    connection ip changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true
 };
 
-export const apiUrl = "http://192.168.0.10/api/";
-export const reportsUrl = "http://192.168.0.10/";
+export const apiUrl = "http://192.168.0.110:80/api/";
+export const reportsUrl = "http://192.168.0.110:80/";
 
 // export const apiUrl = "http://localhost/api/";
 // export const apiUrl = "http://25.39.0.74:90/api/";
